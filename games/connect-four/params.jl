573f75de45a2213c0c1b2039369a9e0a1913fff3
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2020-07-25 22:41:33 -0400

    huge  network

--- games/connect-four/params.jl
+++ games/connect-four/params.jl
@@ -9,15 +9,15 @@ netparams = ResNetHP(
   batch_norm_momentum=0.1)
 
 self_play = SelfPlayParams(
-  num_games=4000,
+  num_games=5000,
   num_workers=128,
   use_gpu=true,
   reset_mcts_every=4,
   mcts=MctsParams(
     num_iters_per_turn=600,
-    cpuct=3.0,
-    prior_temperature=1.0,
-    temperature=PLSchedule([0, 20], [1.0, 0.3]),
+    cpuct=2.0,
+    prior_temperature=0.8,
+    temperature=PLSchedule([0, 15], [1.0, 0.3]),
     dirichlet_noise_ϵ=0.2,
     dirichlet_noise_α=1.0))
 
@@ -27,7 +27,7 @@ arena = ArenaParams(
   use_gpu=true,
   reset_mcts_every=2,
   flip_probability=0.5,
-  update_threshold=0.05,
+  update_threshold=-1.0, # always acccept the new network
   mcts=MctsParams(
     self_play.mcts,
     temperature=ConstSchedule(0.2),
@@ -50,14 +50,13 @@ params = Params(
   arena=arena,
   self_play=self_play,
   learning=learning,
-  num_iters=50,
+  num_iters=20,
   ternary_rewards=true,
   use_symmetries=true,
-  memory_analysis=MemAnalysisParams(
-    num_game_stages=4),
+  memory_analysis=nothing,
   mem_buffer_size=PLSchedule(
   [      0,        40],
-  [400_000, 2_000_000]))
+  [200_000, 1_000_000]))
 
 mcts_baseline =
   Benchmark.MctsRollouts(
